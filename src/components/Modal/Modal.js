a24aca7d10311958f298858aa4e04f7cb5f7ce87
Shahzaib Ibrahim <shahzaib.ibrahim@forwardyou.com>
2022-04-12 23:15:47 +0500

    Fade Slide

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,35 +1,28 @@
 import React from "react";
-import { Transition, CSSTransition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 import "./Modal.css";
 
 const animationTiming = {
-  enter : 400,
-  exit : 1000
-}
+  enter: 400,
+  exit: 400,
+};
 
 const modal = (props) => {
   return (
-    <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClose"
-            : null,
-        ];
-
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
-      }}
-    </Transition>
+    <CSSTransition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+      classNames="fade-slide"
+    >
+      <div className='Modal'>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
